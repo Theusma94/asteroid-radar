e8593b43a6325f0ecff335dc8b147cecfb1a86c5
Matheus Lima <matheuslima9412@gmail.com>
2020-11-02 09:56:27 -0300

    updates fragment to show list of asteroids

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -3,14 +3,20 @@ package com.udacity.asteroidradar.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this, MainViewModel.Factory(requireContext().getString(R.string.API_KEY))).get(MainViewModel::class.java)
+        ViewModelProvider(this,
+            MainViewModel.Factory(
+                requireContext().getString(R.string.API_KEY)
+            )
+        ).get(MainViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -19,9 +25,15 @@ class MainFragment : Fragment() {
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
-
+        val adapter = AsteroidsAdapter {
+            findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
+        }
+        binding.asteroidRecycler.adapter = adapter
         setHasOptionsMenu(true)
 
+        viewModel.resultAsteroids.observe(viewLifecycleOwner, Observer {
+            adapter.submitList(it)
+        })
         return binding.root
     }
 
