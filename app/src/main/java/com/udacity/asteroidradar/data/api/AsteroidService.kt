3607952f91afdf1f720fbbc07e39e5ca118f49bf
Matheus Lima <matheuslima9412@gmail.com>
2020-11-01 22:40:08 -0300

    updates to use provided function instead moshi to parse json

--- app/src/main/java/com/udacity/asteroidradar/data/api/AsteroidService.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/api/AsteroidService.kt
@@ -3,32 +3,34 @@ package com.udacity.asteroidradar.data.api
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Constants
 import kotlinx.coroutines.Deferred
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Url
 
 interface AsteroidService {
     @GET
-    fun getAsteroids(@Url url: String): Deferred<NetworkContainer>
+    fun getAsteroids(@Url url: String): Deferred<String>
 
 //    @GET
 //    fun getImageOfDay(@Query("") query: String): Deferred<>
 }
 
 private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
 private fun client(): OkHttpClient {
     val logging = HttpLoggingInterceptor()
     logging.level = HttpLoggingInterceptor.Level.BODY
     return OkHttpClient.Builder()
-        .addInterceptor(logging)
-        .build()
+            .addInterceptor(logging)
+            .build()
 }
 
 object Network {
@@ -36,11 +38,12 @@ object Network {
 
     // Configure retrofit to parse JSON and use coroutines
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://api.nasa.gov/neo/rest/v1/")
-        .client(client())
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .build()
+            .baseUrl(Constants.BASE_URL)
+            .client(client())
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
 
     val asteroids = retrofit.create(AsteroidService::class.java)
 }
