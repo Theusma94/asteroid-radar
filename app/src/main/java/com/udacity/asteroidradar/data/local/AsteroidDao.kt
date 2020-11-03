5ce6eda916a6aac0b1ba839c5687af9bc08f2d95
Matheus Lima <matheuslima9412@gmail.com>
2020-11-02 23:35:14 -0300

    adds room setup

---
+++ app/src/main/java/com/udacity/asteroidradar/data/local/AsteroidDao.kt
@@ -0,0 +1,18 @@
+package com.udacity.asteroidradar.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.udacity.asteroidradar.data.domain.Asteroid
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AsteroidDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(asteroids: List<AsteroidEntity>)
+
+    @Query("SELECT * FROM asteroid ORDER BY date(closeApproachDate) DESC")
+    fun getAllAsteroids(): Flow<List<AsteroidEntity>>
+}
